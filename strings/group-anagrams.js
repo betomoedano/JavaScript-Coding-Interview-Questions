0201fd201a696de7f044474b5f7aec888e82c729
betomoedano <betomoedano@outlook.com>
2022-08-14 18:11:48 -0400

    add group-anagrams question

---
+++ strings/group-anagrams.js
@@ -0,0 +1,17 @@
+function groupAnagrams(words) {
+  const anagrams = {};
+  for(const word of words) {
+    const sortedWord = word.split('').sort().join('');
+    if(sortedWord in anagrams) {
+      anagrams[sortedWord].push(word);
+    } else {
+      anagrams[sortedWord] = [word];
+    }
+  }
+  return Object.values(anagrams);
+}
+
+const words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"];
+
+console.log(groupAnagrams(words));
+
