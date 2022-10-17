fbc0bd13a3d9a3dc24197b78be1d9715a355c7b8
betomoedano <betomoedano@outlook.com>
2022-10-17 11:04:51 -0400

    add time complexity for group-anagrams

--- strings/group-anagrams.js
+++ strings/group-anagrams.js
@@ -1,8 +1,11 @@
+// time O(w * n * log(n))
+// space O(wn) - where w is the number of words and n is the length of the longest word
+
 function groupAnagrams(words) {
   const anagrams = {};
-  for(const word of words) {
-    const sortedWord = word.split('').sort().join('');
-    if(sortedWord in anagrams) {
+  for (const word of words) {
+    const sortedWord = word.split("").sort().join("");
+    if (sortedWord in anagrams) {
       anagrams[sortedWord].push(word);
     } else {
       anagrams[sortedWord] = [word];
@@ -14,4 +17,3 @@ function groupAnagrams(words) {
 const words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"];
 
 console.log(groupAnagrams(words));
-
