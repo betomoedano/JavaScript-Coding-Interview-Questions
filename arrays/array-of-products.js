f43dee6bc8faef88004222414a65b11e347b6251
betomoedano <betomoedano@outlook.com>
2022-11-06 13:12:29 -0500

    update time

--- arrays/array-of-products.js
+++ arrays/array-of-products.js
@@ -1,3 +1,7 @@
+// Write a function that takes in a non-empty array of integers and returns an
+// array of the same length, where each element in the output array is equal to
+// the product of every other number in the input array.
+
 // time O(n) where n is the length of the input array
 // space O(n)
 
