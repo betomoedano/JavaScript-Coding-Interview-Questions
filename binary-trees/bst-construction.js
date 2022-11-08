3da32f53a68ee0aec38e76753e0a8841f087c114
betomoedano <betomoedano@outlook.com>
2022-11-08 13:15:05 -0500

    add bst construction questiong

---
+++ binary-trees/bst-construction.js
@@ -0,0 +1,97 @@
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  // Time O(log n)
+  // Space O(1) because we are doing the iteration solution
+  insert(value) {
+    let currentNode = this;
+    while (true) {
+      if (value < currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        if (currentNode.right === null) {
+          currentNode.right = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+    }
+    return this;
+  }
+
+  // Time O(log n)
+  // Space O(1) because we are doing the iteration solution
+  contains(value) {
+    let currentNode = this;
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Time O(log n)
+  // Space O(1) because we are doing the iteration solution
+  remove(value, parentNode = null) {
+    let currentNode = this;
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else {
+        if (currentNode.left !== null && currentNode.right !== null) {
+          // means that we have to take the smallest right child
+          currentNode.value = currentNode.right.getMinValue();
+          // then remove
+          currentNode.right.remove(currentNode.value, currentNode);
+        } else if (parentNode === null) {
+          if (currentNode.left !== null) {
+            currentNode.value = currentNode.left.value;
+            currentNode.right = currentNode.left.right;
+            currentNode.left = currentNode.left.left;
+          } else if (currentNode.right !== null) {
+            currentNode.value = currentNode.right.value;
+            currentNode.right = currentNode.right.right;
+            currentNode.left = currentNode.right.left;
+          } else {
+            // this is a single node tree; do nothing.
+          }
+        } else if (parentNode.left === currentNode) {
+          parentNode.left =
+            currentNode.left !== null ? currentNode.left : currentNode.right;
+        } else if (parentNode.right === currentNode) {
+          parentNode.right =
+            currentNode.left !== null ? currentNode.left : currentNode.right;
+        }
+        break;
+      }
+    }
+    return this;
+  }
+
+  getMinValue() {
+    let currentNode = this;
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.value;
+  }
+}
