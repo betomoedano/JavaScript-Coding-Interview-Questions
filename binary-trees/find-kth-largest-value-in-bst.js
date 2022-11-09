2606559ae2fa052d0b860f5f77e0aa589f18456c
betomoedano <betomoedano@outlook.com>
2022-11-09 10:39:52 -0500

    add solution

--- binary-trees/find-kth-largest-value-in-bst.js
+++ binary-trees/find-kth-largest-value-in-bst.js
@@ -12,10 +12,46 @@ function findKthLargestValueInBst(tree, k) {
   return inOrderTraversal[inOrderTraversal.length - k];
 }
 
+// O(n) time
+// O(n) space
 function findKthHelper(tree, array) {
   if (tree === null) return;
   findKthHelper(tree.left, array);
   array.push(tree.value);
   findKthHelper(tree.right, array);
   return array;
+}
+
+// solution 2
+// This is an input class. Do not edit.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class TreeInfo {
+  constructor(numberOfNodesVisited, latestVisitedValue) {
+    this.numberOfNodesVisited = numberOfNodesVisited;
+    this.latestVisitedValue = latestVisitedValue;
+  }
+}
+
+// O(h + k) time | O(h) space - where h is the height of the tree and k is the input parameter
+function findKthLargestValueInBst(tree, k) {
+  const treeInfo = new TreeInfo(0, -1);
+  reverseInOrderTraverse(tree, k, treeInfo);
+  return treeInfo.latestVisitedNodeValue;
+}
+
+function reverseInOrderTraverse(node, k, treeInfo) {
+  if (node === null || treeInfo.numberOfNodesVisited >= k) return;
+  reverseInOrderTraverse(node.right, k, treeInfo);
+  if (treeInfo.numberOfNodesVisited < k) {
+    treeInfo.numberOfNodesVisited++;
+    treeInfo.latestVisitedNodeValue = node.value;
+    reverseInOrderTraverse(node.left, k, treeInfo);
+  }
 }
