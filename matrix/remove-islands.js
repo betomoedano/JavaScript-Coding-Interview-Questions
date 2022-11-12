0a721b1dd209f434a65486164795d17c5658f2f3
betomoedano <betomoedano@outlook.com>
2022-11-11 21:38:57 -0500

    add my solution for remove islands question

--- matrix/remove-islands.js
+++ matrix/remove-islands.js
@@ -1,8 +1,76 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // REMOVE ISLANDS
 
-function removeIslands(matrix) {
+// MY SOLUTION
+/*
+// time O(wh)
+// space O(wh) where wh is the widht and height of the matrix
 
+function removeIslands(matrix) {
+  const visited = matrix.map(row => row.map(col => false));
+
+  for (let col = 0; col < matrix[0].length; col++) {
+    if(matrix[0][col] === 1) {
+     markNonIslands(visited, matrix, 0, col);
+    }
+  } 
+  for (let row = 1; row < matrix.length; row++) {
+    if(matrix[row][0] === 1) {
+     markNonIslands(visited, matrix, row, 0);
+    }
+  }
+  for (let col = 1; col < matrix[0].length; col++) {
+    if(matrix[matrix.length - 1][col] === 1){ 
+     markNonIslands(visited, matrix, matrix.length - 1, col);
+    }
+  }
+  for (let row = 1; row < matrix.length; row++) {
+    if(matrix[row][matrix[0].length - 1] === 1) {
+     markNonIslands(visited, matrix, row ,matrix.length - 1);
+    }
+  }
+
+  for (let row = 1; row < matrix.length; row++) {
+    for (let col = 1; col < matrix[0].length; col++) {
+      if(matrix[row][col] === 1 && visited[row][col] === false) {
+        matrix[row][col] = 0;
+      }
+    }
+  }
+  return matrix;
+}
+
+function markNonIslands(visited, matrix, row, col) {
+  const stack = [[row,col]];
+
+  while (stack.length > 0) {
+    const current = stack.pop();
+    const [row, col] = current;
+    if(visited[row][col]) continue;
+    if (matrix[row][col] === 1) {
+      visited[row][col] = true;
+    }
+    const neighbors = getNeighbors(matrix, row, col);  
+    for (const neighbor of neighbors) {
+      const [row, col] = neighbor;
+      if(visited[row][col]) continue;
+      stack.push(neighbor);
+    }
+  }
+}
+
+function getNeighbors(matrix, row, col) {
+  const neighbors = [];
+  if(row - 1 > 0 && matrix[row - 1][col] === 1) neighbors.push([row - 1, col])
+  if(row + 1 < matrix.length && matrix[row + 1][col] === 1) neighbors.push([row + 1, col]);
+  if(col - 1 > 0 && matrix[row][col - 1] === 1) neighbors.push([row, col - 1])
+  if(col + 1 < matrix[0].length && matrix[row][col + 1] === 1) neighbors.push([row, col + 1]);
+  return neighbors;
+}
+
+*/
+
+function removeIslands(matrix) {
   // 1 -- change ones connected to border to twos '2'
 
   for (let row = 0; row < matrix.length; row++) {
@@ -17,7 +85,7 @@ function removeIslands(matrix) {
   }
 
   // 2 -- change '2' to '1' and '1' to '0';
-  
+
   for (let row = 0; row < matrix.length; row++) {
     for (let col = 0; col < matrix[row].length; col++) {
       const color = matrix[row][col];
@@ -72,7 +140,7 @@ const input = [
   [0, 0, 1, 0, 1, 0],
   [1, 1, 0, 0, 1, 0],
   [1, 0, 1, 1, 0, 0],
-  [1, 0, 0, 0, 0, 1]
+  [1, 0, 0, 0, 0, 1],
 ];
 
 // expected = [
