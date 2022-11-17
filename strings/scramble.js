a5b72c080082e4a0950d00263536ebecacd1a0ff
betomoedano <betomoedano@outlook.com>
2022-11-17 11:18:24 -0500

    add description for scramblies

--- strings/scramble.js
+++ strings/scramble.js
@@ -1,3 +1,9 @@
+// Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+// Notes:
+
+// Only lower case letters will be used (a-z). No punctuation or digits will be included.
+// Performance needs to be considered.
 // O(n) time - where n is the total of chars in str1
 // O(1) space - because at maximum we will have 26 letters in our dictionary
 function scramble(str1, str2) {
