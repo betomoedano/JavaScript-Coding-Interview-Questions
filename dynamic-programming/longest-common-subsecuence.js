c0819282eb4a7ecbf869b6b1d89c41bb6b8d2da2
betomoedano <betomoedano@outlook.com>
2022-11-19 01:38:37 -0500

    add memoization for lcs

--- dynamic-programming/longest-common-subsecuence.js
+++ dynamic-programming/longest-common-subsecuence.js
@@ -1,6 +1,24 @@
+// Add memoization
+// Time O(mn)
+// Space O(nm)
+
+function LCS(i, j, a, b, memo) {
+  let key = i.toString() + "_" + j.toString();
+  if (key in memo) {
+    return memo[key];
+  }
+  if (i >= a.length || j >= b.length) {
+    return 0;
+  } else if (a[i] === b[j]) {
+    memo[key] = 1 + LCS(i + 1, j + 1, a, b, memo);
+  } else {
+    memo[key] = Math.max(LCS(i + 1, j, a, b, memo), LCS(i, j + 1, a, b, memo));
+  }
+  return memo[key];
+}
+
 // This is a pretty terrible solution
 // Time O(2^n)
-// Space O(n)
 function longestCommonSubstring(string1, string2) {
   return LCS(0, 0, string1, string2);
 }
