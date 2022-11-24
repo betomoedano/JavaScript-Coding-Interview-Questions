96e5aefc53bedab9da9b726a72d3f061d64dcf4a
betomoedano <betomoedano@outlook.com>
2022-11-24 15:23:50 -0500

    update binary tree diameter

--- binary-trees/Binary-Tree-Diameter.js
+++ binary-trees/Binary-Tree-Diameter.js
@@ -1,7 +1,8 @@
+// Time O(n) - where n is the number of nodes
+// Space O(h) - average case when the tree is balanced
+// Space O(n) - worst case
 
-/////////////////////////////////////////////////////////////////////////////////////
-// Binary Tree Diamaeter
-
+// This is an input class. Do not edit.
 class BinaryTree {
   constructor(value) {
     this.value = value;
@@ -11,28 +12,32 @@ class BinaryTree {
 }
 
 class TreeInfo {
-  constructor(diameter, height) {
+  constructor(diameter, heigth) {
     this.diameter = diameter;
-    this.height = height;
+    this.height = heigth;
   }
 }
 
 function binaryTreeDiameter(tree) {
-  return getTreeInfo(tree).diameter;
+  return getTreeInfoHelper(tree).diameter;
 }
 
-function getTreeInfo(tree) {
+function getTreeInfoHelper(tree) {
   if (tree === null) return new TreeInfo(0, 0);
 
-  const leftTreeInfo = getTreeInfo(tree.left);
-  const rightTreeInfo = getTreeInfo(tree.right);
+  const leftTreeInfo = getTreeInfoHelper(tree.left);
+  const rightTreeInfo = getTreeInfoHelper(tree.right);
 
-  const longestPathTroughRoot = leftTreeInfo.height + rightTreeInfo.height;
+  // maxDiameterSoFar = max(leftNode.diameter and right.diameter);
+  // largestPossiblePath = left.height + right.height
+  // currentDiameter = max (longestPossiblePath and maxDiameterSoFar)
+  // currentHeight = 1 + max (left.height, right.height);
   const maxDiameterSoFar = Math.max(
     leftTreeInfo.diameter,
     rightTreeInfo.diameter
   );
-  const currentDiameter = Math.max(longestPathTroughRoot, maxDiameterSoFar);
+  const largestPossiblePath = leftTreeInfo.height + rightTreeInfo.height;
+  const currentDiameter = Math.max(maxDiameterSoFar, largestPossiblePath);
   const currentHeight = 1 + Math.max(leftTreeInfo.height, rightTreeInfo.height);
 
   return new TreeInfo(currentDiameter, currentHeight);
@@ -49,4 +54,3 @@ root.left.right.right.right = new BinaryTree(6);
 root.right = new BinaryTree(2);
 
 console.log(binaryTreeDiameter(root));
-
