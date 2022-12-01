d412a959ba1b1c61712c2d535ebd0876a08583e1
betomoedano <betomoedano@outlook.com>
2022-12-01 14:40:16 -0500

    add remove duplicates from linked list

--- linkedLists/remove-duplicates-from-linkedlist.js
+++ linkedLists/remove-duplicates-from-linkedlist.js
@@ -1,3 +1,51 @@
+/**
+ *
+ * @param {LinkedList} myLinked
+ * @returns {LinkedList}
+ */
+// my solution
+// Time O(n) - where n is the number of elements
+// Space O(n)
+function removeDuplicates(myLinked) {
+  if (myLinked === null) return;
+  const frequencies = {};
+
+  let prevNode = null;
+  let currentNode = myLinked;
+
+  while (currentNode !== null) {
+    if (frequencies.hasOwnProperty(currentNode.value)) {
+      prevNode.next = currentNode.next;
+    } else {
+      frequencies[currentNode.value] = currentNode.value;
+      prevNode = currentNode;
+    }
+    currentNode = currentNode.next;
+  }
+  return myLinked;
+}
+
+/**
+ * Using the "Runner" technique
+ * Time O(n^2)
+ * Space O(1)
+ */
+function removeDuplicates(myLinked) {
+  let currentNode = myLinked;
+  while (currentNode !== null) {
+    let runner = currentNode;
+    while (runner.next !== null) {
+      if (runner.next.value === currentNode.value) {
+        runner.next = runner.next.next;
+      } else {
+        runner = runner.next;
+      }
+    }
+    currentNode = currentNode.next;
+  }
+  return myLinked;
+}
+
 // time O(n) where n is the number of nodes
 // space O(1)
 function removeDuplicatesFromLinkedList(linkedList) {
