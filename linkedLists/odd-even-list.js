ae4372e4d02a57c8eb2256a2312079fabad4f7b3
betomoedano <betomoedano@outlook.com>
2022-12-06 18:17:19 -0500

    add odd even list

---
+++ linkedLists/odd-even-list.js
@@ -0,0 +1,18 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function oddEvenList(head) {
+  if (!head) return head;
+
+  var odd = head;
+  var even = head.next;
+  while (odd.next && odd.next.next) {
+    var tmp = odd.next;
+    odd.next = odd.next.next;
+    odd = odd.next;
+    tmp.next = odd.next;
+  }
+  odd.next = even;
+  return head;
+}
