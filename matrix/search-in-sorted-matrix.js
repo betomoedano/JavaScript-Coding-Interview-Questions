b729e72f2588ff167f6e9588f7750c28850401aa
betomoedano <betomoedano@outlook.com>
2022-12-11 11:52:12 -0500

    update

--- matrix/search-in-sorted-matrix.js
+++ matrix/search-in-sorted-matrix.js
@@ -8,7 +8,7 @@
 // function searchInSortedMatrix(matrix, target) {
 //     const result = [-1, -1];
 //     let hasTarget = false;
-  
+
 //     for (let row = 0; row < matrix.length; row++) {
 //       hasTarget = binarySearch(matrix[row], target);
 //       if (hasTarget[0]) {
@@ -19,7 +19,7 @@
 //     }
 //     return result;
 //   }
-  
+
 //   function binarySearch(row, target) {
 //     let left = 0;
 //     let right = row.length - 1;
@@ -38,33 +38,31 @@
 //   }
 
 // solution 2
-
 // time O(n+m)
 // space O(1)
 
 function searchInSortedMatrix(matrix, target) {
+  let row = 0;
+  let col = matrix[0].length - 1;
 
-    let row = 0;
-    let col = matrix[0].length - 1;
-
-    while(row < matrix.length && col >= 0) {
-        if(matrix[row][col] > target) {
-            col--;
-        } else if(matrix[row][col] < target) {
-            row++;
-        } else {
-            return [row, col];
-        }
+  while (row < matrix.length && col >= 0) {
+    if (matrix[row][col] > target) {
+      col--;
+    } else if (matrix[row][col] < target) {
+      row++;
+    } else {
+      return [row, col];
     }
-    return [-1, -1];
+  }
+  return [-1, -1];
 }
-  
-  const matrix = [
-    [1, 4, 7, 12, 15, 1000],
-    [2, 5, 19, 31, 32, 1001],
-    [3, 8, 24, 33, 35, 1002],
-    [40, 41, 42, 44, 45, 1003],
-    [99, 100, 103, 106, 128, 1004]
-  ];
-  
-  console.log(searchInSortedMatrix(matrix, 44));
+
+const matrix = [
+  [1, 4, 7, 12, 15, 1000],
+  [2, 5, 19, 31, 32, 1001],
+  [3, 8, 24, 33, 35, 1002],
+  [40, 41, 42, 44, 45, 1003],
+  [99, 100, 103, 106, 128, 1004],
+];
+
+console.log(searchInSortedMatrix(matrix, 44));
