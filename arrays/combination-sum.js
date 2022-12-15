f5ef45c2e3816f5d639f38db486ab7a6e315da0e
betomoedano <betomoedano@outlook.com>
2022-12-15 18:39:32 -0500

    add combinations sum 2

--- arrays/combination-sum.js
+++ arrays/combination-sum.js
@@ -46,3 +46,41 @@ function combinationHelper(
     uniqueCombinations
   );
 }
+
+//////////////////////////////////////////////
+// Combination Sum 2
+// No duplicates allowed
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function (candidates, target) {
+  candidates.sort((a, b) => a - b);
+  let paths = [];
+
+  find(target, [], 0, candidates, paths);
+
+  return paths;
+};
+
+// Time O(2^n) | Space O(n)
+function find(target, currPath, index, candidates, paths) {
+  if (target === 0) {
+    paths.push(currPath.slice());
+    return;
+  } else {
+    while (index < candidates.length && target - candidates[index] >= 0) {
+      find(
+        target - candidates[index],
+        [...currPath, candidates[index]],
+        index + 1,
+        candidates,
+        paths
+      );
+      index++;
+      while (candidates[index - 1] === candidates[index]) index++;
+    }
+  }
+}
