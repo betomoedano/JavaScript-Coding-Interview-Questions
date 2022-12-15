8281aabe64c2474480cba0c81e1b785a1e76bcd5
betomoedano <betomoedano@outlook.com>
2022-12-15 14:56:52 -0500

    add solution for 3num sum

--- arrays/three-num-sum.js
+++ arrays/three-num-sum.js
@@ -28,3 +28,47 @@ function threeNumberSum(array, targetSum) {
 
 const array = [12, 3, 1, 2, -6, 5, 0, -8, -1, 6];
 const targetSum = 0;
+
+// This solution avoids duplicates
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+// Time O(n^2)
+// Space O(n)
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let triplets = [];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] == nums[i - 1]) continue; //avoid duplicates
+
+    let leftPointer = i + 1;
+    let rightPointer = nums.length - 1;
+
+    while (leftPointer < rightPointer) {
+      let currentSum = nums[i] + nums[leftPointer] + nums[rightPointer];
+      if (currentSum === 0) {
+        triplets.push([nums[i], nums[leftPointer], nums[rightPointer]]);
+        leftPointer++;
+        rightPointer--;
+        while (
+          leftPointer < rightPointer &&
+          nums[leftPointer] == nums[leftPointer - 1]
+        )
+          leftPointer++; //avoid duplicates
+        while (
+          leftPointer < rightPointer &&
+          nums[rightPointer] == nums[rightPointer + 1]
+        )
+          rightPointer--; //avoid duplicates
+      } else if (currentSum > 0) {
+        rightPointer--;
+      } else if (currentSum < 0) {
+        leftPointer++;
+      }
+    }
+  }
+  return triplets;
+};
