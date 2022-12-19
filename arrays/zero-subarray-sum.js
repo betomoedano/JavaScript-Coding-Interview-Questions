0dda627c968e754a6c90aa764e7c581537615633
betomoedano <betomoedano@outlook.com>
2022-12-19 15:11:25 -0500

    add zero subarray sum

---
+++ arrays/zero-subarray-sum.js
@@ -0,0 +1,12 @@
+// Time O(n) | Space O(n) - where n is the length of nums
+function zeroSumSubarray(nums) {
+  const set = new Set([0]);
+  let currSum = 0;
+
+  for (let num of nums) {
+    currSum += num;
+    if (set.has(currSum)) return true;
+    set.add(currSum);
+  }
+  return false;
+}
