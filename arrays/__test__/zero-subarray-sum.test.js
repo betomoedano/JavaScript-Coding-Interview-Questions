8d97fa5105f8c3b32de9d70f1b95b65799c88a51
betomoedano <betomoedano@outlook.com>
2022-12-19 15:33:43 -0500

    add test for zero subarray sum

--- arrays/__test__/zero-subarray-sum.test.js
+++ arrays/__test__/zero-subarray-sum.test.js
@@ -1,5 +1,20 @@
 const zeroSumSubarray = require("../zero-subarray-sum");
 
-test("there is a 0 sub sum", () => {
+test("given [1, 2, -2] return true", () => {
   expect(zeroSumSubarray([1, 2, -2])).toBe(true);
+});
+
+test("given [] return false", () => {
+  expect(zeroSumSubarray([])).toBe(false);
+});
+
+test("given [0] return true", () => {
+  expect(zeroSumSubarray([0])).toBe(true);
+});
+test("given [-8, -22, 104, 73, -120, 53, 22, 20, 25, -12, 1, 14, -90, 13, -22] return true", () => {
+  expect(
+    zeroSumSubarray([
+      -8, -22, 104, 73, -120, 53, 22, 20, 25, -12, 1, 14, -90, 13, -22,
+    ])
+  ).toBe(true);
 });
