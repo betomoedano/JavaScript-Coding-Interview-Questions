d10024cec9d95650e17c62986bea216a167f49fe
betomoedano <betomoedano@outlook.com>
2022-12-28 00:35:47 -0500

    add time and space complexity to route between nodes

--- graphs/route-between-nodes.js
+++ graphs/route-between-nodes.js
@@ -1,6 +1,8 @@
 /**
  * Given a directed graph and two nodes S and E.
  * Design an algorithm to find out whether there is a route from S to E
+ * Time O(v + e) - where v are the vertices and e the edges in the graph
+ * Space O(v) - because in the worst case we will have all vetices in the stack
  */
 
 function routeBetweenNodes(graph, origin, destination) {
