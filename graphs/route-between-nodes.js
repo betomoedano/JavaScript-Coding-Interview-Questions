8a4d1ab6cd4b9e7333916f5ace60ce60c95e681c
betomoedano <betomoedano@outlook.com>
2022-12-28 00:09:10 -0500

    add route between nodes

---
+++ graphs/route-between-nodes.js
@@ -0,0 +1,28 @@
+/**
+ * Given a directed graph and two nodes S and E.
+ * Design an algorithm to find out whether there is a route from S to E
+ */
+
+function routeBetweenNodes(graph, origin, destination) {
+  const stack = [origin];
+
+  while (stack.length > 0) {
+    const currentNode = stack.pop();
+
+    for (const node of graph[currentNode]) {
+      if (node === destination) return true;
+      if (node === currentNode) continue;
+      stack.push(node);
+    }
+  }
+  return false;
+}
+
+const graph = {
+  1: [2],
+  2: [3, 4],
+  3: [4],
+  4: [],
+};
+
+console.log(routeBetweenNodes(graph, 1, 4));
