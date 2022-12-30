ecdcbc500378adec1647ab33e10e6b9d0cc87860
betomoedano <betomoedano@outlook.com>
2022-12-30 15:30:28 -0500

    add list of depths question

--- binary-trees/list-of-depths.js
+++ binary-trees/list-of-depths.js
@@ -1,24 +1,10 @@
+const LinkedList = require("../utils/LinkedList");
 /**
  * Given a binary tree design an algorithm wich creates
  * a linked list of all the nodes at each depth (e.g., if you
  * have a tree with depth D, you'll have D linked lists).
  */
 
-class LinkedList {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-
-  add(value) {
-    let currentNode = this;
-
-    while (currentNode !== null) {
-      currentNode = currentNode.next;
-    }
-    currentNode.next = new LinkedList(value);
-  }
-}
 class Node {
   constructor(value) {
     this.value = value;
@@ -26,7 +12,39 @@ class Node {
     this.right = null;
   }
 }
+function listOfDepths(root) {
+  const listOfLists = [];
+  let list = null;
+  let newNode = null;
+  let queue = [];
+  let nextQueue = [];
+  let currentNode = root;
 
+  queue.unshift(currentNode);
+
+  while (queue.length > 0) {
+    currentNode = queue.pop();
+    if (currentNode === null) continue;
+    newNode = new LinkedList(currentNode.value);
+    newNode.next = list;
+    list = newNode;
+    if (currentNode.left !== null) {
+      nextQueue.unshift(currentNode.left);
+    }
+    if (currentNode.rigth !== null) {
+      nextQueue.unshift(currentNode.right);
+    }
+    if (queue.length === 0) {
+      listOfLists.push(list);
+      list = null;
+      queue = nextQueue;
+      nextQueue = [];
+    }
+  }
+  return listOfLists;
+}
+
+// TEST
 const tree = new Node(1);
 tree.right = new Node(3);
 tree.left = new Node(2);
@@ -34,3 +52,5 @@ tree.left.left = new Node(4);
 tree.left.right = new Node(5);
 tree.right.right = new Node(7);
 tree.right.left = new Node(6);
+
+console.log(listOfDepths(tree));
