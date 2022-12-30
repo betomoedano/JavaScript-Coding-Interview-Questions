4414bb2838920e67fb4026fe85d11af9144f01d7
betomoedano <betomoedano@outlook.com>
2022-12-29 22:08:16 -0500

    add minimal tree question

--- binary-trees/minimal-tree.js
+++ binary-trees/minimal-tree.js
@@ -18,13 +18,16 @@ function createMinimalBST(arr) {
 }
 
 function createMinimalBSTHelper(arr, start, end) {
-  if (end < start) return;
+  if (end < start) return null;
+
   const middle = Math.floor((start + end) / 2);
 
   const node = new Node(arr[middle]);
-  node.left(createMinimalBSTHelper(arr, 0, middle - 1));
-  node.right(createMinimalBSTHelper(arr, middle + 1, arr.length - 1));
+  node.left = createMinimalBSTHelper(arr, start, middle - 1);
+  node.right = createMinimalBSTHelper(arr, middle + 1, end);
   return node;
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(createMinimalBST(array));
