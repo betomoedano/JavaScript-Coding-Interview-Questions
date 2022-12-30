36adbd2c8d833b57b8ee45f4c491bc16222cdc89
betomoedano <betomoedano@outlook.com>
2022-12-29 22:24:58 -0500

    add test for minimal tree

--- binary-trees/minimal-tree.js
+++ binary-trees/minimal-tree.js
@@ -31,6 +31,4 @@ function createMinimalBSTHelper(arr, start, end) {
   return node;
 }
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-console.log(createMinimalBST(array));
+module.exports = createMinimalBST;
