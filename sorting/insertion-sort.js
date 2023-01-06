a894bd7b27a39f75d3284ffcc4a30ed93502d3b8
betomoedano <betomoedano@outlook.com>
2023-01-06 15:33:18 -0500

    add variant insertion sort

--- sorting/insertion-sort.js
+++ sorting/insertion-sort.js
@@ -18,3 +18,20 @@ function swap(a, b, array) {
   array[a] = array[b];
   array[b] = temp;
 }
+
+// variation with one-liner swap
+//
+// Time O(n) - Best Case
+// Time O(n^2) - Worst Case
+// Space O(1)
+//
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      [array[j], array[j - 1]] = [array[j - 1], array[j]];
+      j--;
+    }
+  }
+  return array;
+}
