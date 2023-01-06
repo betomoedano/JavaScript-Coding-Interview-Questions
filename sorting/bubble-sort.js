4365265a997846383ec205dc57e8ca58f2b722e7
betomoedano <betomoedano@outlook.com>
2023-01-06 15:14:05 -0500

    add a less optimized bubble sort algorithm

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,6 +1,12 @@
+//
+// This is a little more optimized, it is likely to be faster in practice because
+// it short-circuits the inner loop and breaks out of the outer loop early
+// when the array is already sorted.
 // Best O(n) time
 // Time O(n^2) -- because we need to iterate multiple times
 // Space O(1)
+//
+
 function bubbleSort(array) {
   let isSorted = false;
   let iteration = 0;
@@ -26,3 +32,18 @@ function swap(a, b, array) {
 }
 const nums = [1, 3, 4, 5, 67, 4, 2];
 console.log(bubbleSort(nums));
+
+// not optimized version
+// same time-space complexity
+
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        // swap
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
+}
