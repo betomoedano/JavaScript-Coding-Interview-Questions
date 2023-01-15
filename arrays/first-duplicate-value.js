5cd5e853c23996c62ac1634e0bae346f2a790c76
betomoedano <betomoedano@outlook.com>
2023-01-15 12:15:45 -0500

    add explanation and less optimal solution

--- arrays/first-duplicate-value.js
+++ arrays/first-duplicate-value.js
@@ -9,13 +9,35 @@
 // If no integer appears more than once, your function should return - 1
 // <p>Note that you're allowed to mutate the input array.</p>
 
+//
 // Time O(n)
 // Space O(1)
+// We are using the fact that the array only contains values
+// between 1 - n inclusive. We convert the values that we've already
+// seen to negative that way when we find a value that is negative it
+// means we have found the first duplicated value.
+//
 function firstDuplicateValue(array) {
   for (const value of array) {
-    const absValue = Math.abs(value);
-    if (array[absValue - 1] < 0) return absValue;
-    array[absValue - 1] *= -1;
+    const idx = Math.abs(value) - 1;
+    if (array[idx] < 0) return Math.abs(value);
+    array[idx] *= -1;
+  }
+  return -1;
+}
+
+//
+// Time O(n)
+// Space O(n)
+// This solutions is a bit worse than the one above because
+// we are using extra space by creating the set of values that we've already seen
+//
+function firstDuplicateValue(array) {
+  const set = new Set();
+
+  for (const num of array) {
+    if (set.has(num)) return num;
+    set.add(num);
   }
   return -1;
 }
