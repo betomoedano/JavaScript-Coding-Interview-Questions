35a39d94d2ebb83b729c16eec7f630590a21f7ee
betomoedano <betomoedano@outlook.com>
2023-01-19 16:01:23 -0500

    add iterative solution for validate bst question

--- binary-trees/validate-bst.js
+++ binary-trees/validate-bst.js
@@ -17,3 +17,37 @@ function validateBstHelper(tree, minValue, maxValue) {
   const leftIsValid = validateBstHelper(tree.left, minValue, tree.value);
   return leftIsValid && validateBstHelper(tree.right, tree.value, maxValue);
 }
+
+//
+// SOLUTION 2
+// This solution is iterative, same time complexity using DFS approach
+// Time O(n)
+// Space O(d) - where d is the depth of the tree
+//
+function validateBst(root) {
+  if (root === null) return false;
+  const stack = [root];
+  root.min = -Infinity;
+  root.max = Infinity;
+
+  while (stack.length > 0) {
+    const currentNode = stack.pop();
+    if (
+      currentNode === null ||
+      currentNode.value < currentNode.min ||
+      currentNode.value >= currentNode.max
+    )
+      return false;
+    if (currentNode.left) {
+      currentNode.left.min = currentNode.min;
+      currentNode.left.max = currentNode.value;
+      stack.push(currentNode.left);
+    }
+    if (currentNode.right !== null) {
+      stack.push(currentNode.right);
+      currentNode.right.min = currentNode.value;
+      currentNode.right.max = currentNode.max;
+    }
+  }
+  return true;
+}
