9f1c6e70be1a69b7c441c6928a3a07262d4ad96e
betomoedano <betomoedano@outlook.com>
2023-01-23 20:46:18 -0500

    add leetcode 997

--- arrays/997-find-the-town-judge.js
+++ arrays/997-find-the-town-judge.js
@@ -1,3 +1,28 @@
+// 997. Find the Town Judge
+// Easy
+// In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+
+// If the town judge exists, then:
+
+// The town judge trusts nobody.
+// Everybody (except for the town judge) trusts the town judge.
+// There is exactly one person that satisfies properties 1 and 2.
+// You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi.
+
+// Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+
+// Example 1:
+
+// Input: n = 2, trust = [[1,2]]
+// Output: 2
+// Example 2:
+
+// Input: n = 3, trust = [[1,3],[2,3]]
+// Output: 3
+// Example 3:
+
+// Input: n = 3, trust = [[1,3],[2,3],[3,1]]
+// Output: -1
 /**
  * @param {number} n
  * @param {number[][]} trust
